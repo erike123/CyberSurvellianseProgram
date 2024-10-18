85c759c745b591aeacea36d62616ea8db004d1e1
ERoydev <e.roydev@gmail.com>
2024-10-18 19:43:36 +0300

    formDataModel on backend

---
+++ Models/FormDataModel.cs
@@ -0,0 +1,23 @@
+namespace Web3Auditor.Models
+{
+	public class FormDataModel
+	{
+		public string projectTitle { get; set; }
+
+		public string projectType { get; set; }
+
+		public string repositoryLink { get; set; }
+
+		public string technologies { get; set; }
+
+		public double budget { get; set; }
+
+		public string deadline { get; set; }
+
+		public string priorityLevel { get; set; }
+
+		public string desiredFreelancerExperienceLevel { get; set; }
+
+		public string projectDescription { get; set; }
+	}
+}
