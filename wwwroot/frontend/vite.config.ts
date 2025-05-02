7cf10b4f61dbb97dbbe10c267d45e47cfc1d82e0
Erik <email33@abv.bg>
2025-05-03 00:49:32 +0300

    Refactor controller structure and update frontend

--- wwwroot/frontend/vite.config.ts
+++ wwwroot/frontend/vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     host: '0.0.0.0'
   }
-=======
->>>>>>> 40c7dfb047049537d13840c31c1d7f071d587d5c
 })
